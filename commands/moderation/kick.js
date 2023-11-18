ce0b4163b556d0a43a15845922691b415b9965c4
curly <luckahudson10@gmail.com>
2023-11-18 14:01:40 -0600

    Finalized kick command 😭

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,4 +1,4 @@
-const { PermissionsBitField, MessageEmbed } = require('discord.js');
+const { PermissionsBitField, EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: 'kick',
@@ -9,7 +9,22 @@ module.exports = {
   example: "@user reason",
   usage: "(member) <reason>",
   execute(message, args) {
-    const targetUser = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+    let targetUser = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+
+    if (!targetUser) {
+      const username = args.join(' ').toLowerCase();
+      const foundMember = message.guild.members.cache.find((member) =>
+        member.user.username.toLowerCase() === username ||
+        member.displayName.toLowerCase() === username
+      );
+    
+      if (foundMember) {
+        targetUser = foundMember;
+      } else {
+        return ctx.warn('User not found. Please mention a valid user or provide a valid user ID.');
+      }
+    }
+
     const targetMember = message.guild.members.cache.get(targetUser.id);
 
     if (!targetMember) {
@@ -26,32 +41,35 @@ module.exports = {
 
     const reason = args.slice(1).join(' ') || 'No reason given';
 
-    targetMember.kick(reason)
+    const embed = new EmbedBuilder()
+      .setTitle('**Kicked**')
+      .setDescription(`> You've been kicked from ${message.guild.name}`)
+      .addFields({ name: `**Moderator**`, value: `${message.author.tag}`, inline: true })
+      .addFields({ name: `**Reason**`, value: `${reason}`, inline: true })
+      .setColor("#2f3136")
+      .setThumbnail(message.guild.iconURL({ dynamic: true, size: 2048 }))
+      .setAuthor({ name: message.guild.name, iconURL: message.guild.iconURL({ dynamic: true, size: 2048 })});  
+
+    targetUser.send({ embeds: [embed] })
       .then(() => {
-        const embed = new MessageEmbed()
-        .setTitle('**Kicked**')
-        .setDescription(`> You've been kicked from ${message.guild.name}`)
-        .addFields({ name: `**Moderator**`, value: `${message.author.tag}`}, true)
-        .addFields({ name: `**Reason**`, value: `${reason}`}, true)
-        .setColor("#2f3136")
-        .setThumbnail(message.guild.iconURL({ dynamic: true, size: 2048 }))
-        .setAuthor(message.guild.name, message.guild.iconURL({ dynamic: true, size: 2048 }));  
-
-        ctx.approve(`**${targetUser.user.tag}** has been kicked. Reason: ${reason}`);
-        message.reply({ embeds: [embed] });
-
-        targetUser.send({ embeds: [embed] })
-          .catch(err => console.error(`Failed to send DM: ${err}`));
+        targetMember.kick(reason)
+          .then(() => {
+            ctx.approve(`**${targetUser.user.tag}** has been kicked. Reason: ${reason}`);
+          })
+          .catch(err => {
+            console.error(err);
+            if (err.code === 50013) {
+              return ctx.warn(`I do not have the necessary permissions to **${targetMember.user.tag}**.`);
+            } else if (err.code === 50051) {
+              return ctx.warn(`I cannot kick ${targetMember.user.tag} due to role hierarchy.`);
+            } else {
+              ctx.warn(`An error occurred while trying to kick the user.`);
+            }
+          });
       })
       .catch(err => {
-        console.error(err);
-        if (err.code === 50013) {
-          return ctx.warn(`I do not have the necessary permissions to **${targetMember.user.tag}**.`);
-        } else if (err.code === 50051) {
-          return ctx.warn(`I cannot kick ${targetMember.user.tag} due to role hierarchy.`);
-        } else {
-          ctx.error(`An error occurred while trying to kick the user.`);
-        }
+        console.error(`Failed to send DM: ${err}`);
+        ctx.warn(`An error occurred while trying to send a DM to the user.`);
       });
   },
 };
