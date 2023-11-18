ecf8e9c4c72ab0aaf64ecd3c8d3b12db274ca80b
curly <luckahudson10@gmail.com>
2023-11-18 09:36:45 -0600

    nothing you should worry about

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -6,6 +6,7 @@ module.exports = {
     name: 'eval',
     description: 'Evaluate JavaScript or Python code',
     ownerOnly: true,
+    send: false,
     execute(message, args) {
         // Check if the user executing the command is the owner
         if (message.author.id === '1068177499231621270') {
