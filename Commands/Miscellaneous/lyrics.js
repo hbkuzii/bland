706b100590b2b14945193858b49399e9c4ae50a0
curly <luckahudson10@gmail.com>
2023-11-19 07:47:47 -0600

    added lyrics command

--- Commands/Miscellaneous/lyrics.js
+++ Commands/Miscellaneous/lyrics.js
@@ -2,37 +2,49 @@ const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('
 const config = require('../../config.json');
 const { ContextMenuCommandAssertions } = require('discord.js');
 const Genius = require("genius-lyrics");
-
 module.exports = {
     name: 'lyrics',
     description: 'Search for lyrics of a song',
     usage: '(song-name)',
     permissions: ['SendMessages'],
-    execute(message, args) {
-        if (!args[0]) {
-            // Handle case where no song name is provided
-            message.channel.send('Please provide the name of the song.');
-            return;
-        }
-
+    async execute(message, args) {
         const songName = args.join(' ');
 
         const Client = new Genius.Client(process.env.GENIUS_ACCESS);
-        const searches = Client.songs.search(songName);
+        const searches = await Client.songs.search(songName);
 
-        if (!searches || searches.length === 0) {
-            // Handle case where no results are found
-            message.channel.send('No lyrics found for the given song.');
+        if (!searches[0]) {
+            ctx.warn('No lyrics found for that song.');
             return;
         }
 
         const song = searches[0];
-        const lyrics = song.lyrics();
+        const lyrics = await song.lyrics();
 
-        // Process and send lyrics, similar to your existing code
-        // ...
+        const embeds = await Promise.all(lyrics.match(/[\s\S]{1,2048}/g).map((x) => {
+            return new EmbedBuilder()
+                .setAuthor({
+                    name: message.member.displayName,
+                    iconURL: message.author.displayAvatarURL(),
+                })
+                .setTitle(`${song.artist.name} - ${song.title}`)
+                .setURL(song.url)
+                .setDescription(x)
+                .setThumbnail(song.image)
+                .setColor(config.color);
+        }));
 
-        // Example: Send the first 800 characters of lyrics
-        message.channel.send(lyrics.substring(0, 800));
-    },
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setLabel('Genius')
+                    .setStyle(ButtonStyle.Link)
+                    .setURL(song.url)
+            );
+
+        message.reply({
+            embeds: embeds,
+            components: [row],
+        });
+    }
 };
