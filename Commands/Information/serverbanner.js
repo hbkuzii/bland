059714f8a837fe60e21eed423b3faafe36b64dcb
curly <luckahudson10@gmail.com>
2023-12-12 11:06:18 -0600

    fff

---
+++ Commands/Information/serverbanner.js
@@ -0,0 +1,27 @@
+const { EmbedBuilder } = require('discord.js');
+const config = require("../../config.json")
+
+module.exports = {
+  name: "serverbanner",
+  aliases: ["sb", "sbanner"],
+  category: "information",
+  description: 'Get the server banner',
+  send: false,
+  permissions: ['SendMessages'],
+  async execute(message) {
+    let banner = message.guild.bannerURL({ dynamic: true, format: "png", size: 2048 });
+
+    if (banner) {
+      const embed = new EmbedBuilder()
+        .setColor(config.color)
+        .setTitle(`${message.guild.name}'s server banner`)
+         .setURL(banner)
+        .setImage(banner)
+
+
+      message.channel.send({ embeds: [embed] });
+    } else {
+      ctx.warn("This server does not have a banner.");
+    }
+  }
+}
---
+++ Commands/Information/servericon.js
@@ -0,0 +1,27 @@
+const { EmbedBuilder } = require('discord.js');
+const config = require("../../config.json")
+
+module.exports = {
+  name: "servericon",
+  aliases: ["sicon", "guildbanner"],
+  category: "information",
+  description: 'Get the server banner',
+  send: false,
+  permissions: ['SendMessages'],
+  async execute(message) {
+    let icon = message.guild.iconURL({ dynamic: true, format: "png", size: 2048 });
+
+    if (icon) {
+      const embed = new EmbedBuilder()
+        .setColor(config.color)
+        .setTitle(`${message.guild.name}'s server icon`)
+         .setURL(icon)
+        .setImage(icon)
+
+
+      message.channel.send({ embeds: [embed] });
+    } else {
+      ctx.warn("This server does not have a icon.");
+    }
+  }
+}
